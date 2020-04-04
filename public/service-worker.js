4b918afbe1b1ab1bd29cee46852e68b6129f7c7b
Ryan <ryandub9@gmail.com>
2020-04-04 15:59:55 -0700

    added db.js and started service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/db.js",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+  ];
