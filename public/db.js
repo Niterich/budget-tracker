d3c88394622fea5c708cdf8fc6182f7e3aa08e73
Ryan <ryandub9@gmail.com>
2020-04-04 22:51:34 -0700

    removed some consolelogs

--- public/db.js
+++ public/db.js
@@ -28,7 +28,6 @@ function checkingDB() {
     getAll.onsuccess = function () {
         console.log(getAll.result);
         if (getAll.result.length > 0) {
-            console.log(getAll.result);
             fetch("/api/transaction/bulk", {
                 method: "POST",
                 body: JSON.stringify(getAll.result),
